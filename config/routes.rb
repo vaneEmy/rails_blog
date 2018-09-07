a0c0266c2efc1658cc963230b1b6c81e888b8322
vaneemy <vanvanemily@gmail.com>
2018-09-07 02:43:42 -0500

    Adding articles resources.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/home'
   get 'welcome/about'
 
+  resources :articles
   root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
