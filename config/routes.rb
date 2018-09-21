c957bec6e997df4562b18fdfb45a4d2174aafee9
vaneemy <vanvanemily@gmail.com>
2018-09-21 01:31:11 -0500

    Adding users resources.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'welcome#home'
 
   get 'signup', to: 'users#new'
-  post 'users', to: "users#create"
+
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
