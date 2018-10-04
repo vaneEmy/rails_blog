85476bd53944025ba2b974a32379a22f77890304
vaneemy <vanvanemily@gmail.com>
2018-10-04 01:46:31 -0500

    Updating the schema with the new admin column.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_20_055601) do
+ActiveRecord::Schema.define(version: 2018_10_04_061046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2018_09_20_055601) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
