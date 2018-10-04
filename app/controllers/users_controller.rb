17987ce3ed7a07e322a01e10e1135d44010ec99d
vaneemy <vanvanemily@gmail.com>
2018-10-04 01:48:28 -0500

    Creating a destroy method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_admin, only: [:destroy]
 
     def index
         @users = User.paginate(page: params[:page], per_page: 3)
     end
+    
     def new
         @user = User.new
     end
@@ -36,18 +38,32 @@ class UsersController < ApplicationController
         @user_articles = @user.articles.paginate(page: params[:page], per_page:3)
     end
 
+    def destroy 
+        @user = User.find(params[:id])
+        @user.destroy
+        flash[:danger] = "User and all articles created by user have been deleted"
+        redirect_to users_path
+    end
 
     private
         def user_params
             params.require(:user).permit(:username, :email, :password)
         end
+
         def set_user
             @user = User.find(params[:id])
         end
+        
         def require_same_user
-            if current_user != @user
+            if current_user != @user and !current_user.admin?
                 flash[:danger] = "You can only edit your own account"
                 redirect_to root_path
+            end
+        end
+
+        def require_admin
+            if logged_in? and !current_user.admin?
+                flash[:danger] = "Onl admin users can perform that action"
             end
         end
 end
