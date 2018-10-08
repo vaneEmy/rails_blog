0e1eae5cb7c8ab6067a06cea34481bbafaf808a0
vaneemy <vanvanemily@gmail.com>
2018-10-07 23:55:17 -0500

    Adding an Integration test for invalid category.

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -11,4 +11,15 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         assert_template 'categories/index'
         assert_match "sports", response.body
    end  
+
+   test "invalid category submission results in failure" do 
+        get new_category_path
+        assert_template 'categories/new'
+        assert_no_difference 'Category.count' do 
+            post categories_path, params: { category: { name: " " } }
+        end
+        assert_template 'categories/new'
+        assert_select 'h2.alert-heading'
+        assert_select 'div.mb-0'
+   end
 end
