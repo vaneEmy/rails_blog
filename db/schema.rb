8b35352c2cf06d8114133f4cf6021f4e93e00d7a
vaneemy <vanvanemily@gmail.com>
2018-10-20 00:53:41 -0500

    Updating schema for ArticleCategory table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_07_041059) do
+ActiveRecord::Schema.define(version: 2018_10_20_053340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
