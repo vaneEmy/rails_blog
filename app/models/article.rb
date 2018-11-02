de6fd7a3c7ebd793f05dc4f3b840a13e11ce09af
vaneEmy <vanvanemily@gmail.com>
2018-11-01 23:57:19 -0600

    Change validation for lenght of the title in articles.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
     belongs_to :user
     has_many :article_categories
     has_many :categories, :through =>  :article_categories
-    validates :title, presence: true, length: { minimum: 3, maximum: 15 }
-    validates :description, presence: true, length: {minimum: 10, maximum: 100 }
+    validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+    validates :description, presence: true, length: {minimum: 10, maximum: 120 }
     validates :user_id, presence: true
 end
